15f7d3ee03d215b0e4b292ecc5efe95e2fd7446a
yashyadavMRU <yashyadav8595411@gmail.com>
2026-02-25 19:44:15 +0530

    added login and logout feature

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -61,16 +61,31 @@ export const login = async(req,res) => {
         }
         const user = await User.findOne({email});
 
-        if(user){
+        if(!user) return res.status(400).json({message: "Invalid credentials"});
 
-        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials"});
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
     } catch (error) {
         console.log("Error in Login controller", error);
-        res.status(400).json({ message: "Internal Server Error"});
+        res.status(500).json({ message: "Internal Server Error"});
     }
 
 };
 
-export const logout = (req,res) => {
-    res.send("Logout endpoint");
+export const logout = (_,res) => {
+    res.cookie("jwt", "", {
+        maxAge: 0,
+    });
+    res.status(200).json({message: "Logged out successfully"});
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,9 +3,9 @@ import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", signup);
+router.post("/signup", signup);
 
-router.get("/login", login);
+router.post("/login", login);
 
 router.get("/logout", logout);
 
