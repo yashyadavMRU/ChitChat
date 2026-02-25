8433d5ec56c2e5f21b3d88ab3880532e9f4a7fd8
yashyadavMRU <yashyadav8595411@gmail.com>
2026-02-25 18:59:56 +0530

    Signup feature added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,71 @@
+
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async(req,res) => {
+
+    const {fullName, email, password} = req.body;
+    try {
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be at least 6 characters"});
+        }
+
+        // check if email is valid using regx
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message: "Invalid email format"});
+        }
+
+        const user = await User.findOne({email});
+
+        if(user) return res.status(400).json({message: "Email already exists."});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName, email, password: hashedPassword
+        });
+
+        if(newUser){
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email:newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        }else{
+            res.status(400).json({message: "Invalid User data"});
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export const login = async(req,res) => {
+    const {email, password} = req.body;
+
+    try {
+        if(!email || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }
+    } catch (error) {
+        console.log("Error in Login controller", error);
+        res.status(400).json({ message: "Internal Server Error"});
+    }
+
+};
+
+export const logout = (req,res) => {
+    res.send("Logout endpoint");
+};
