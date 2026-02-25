f9f609752e4d0ef4b55addc557a933d66a99316e
yashyadavMRU <yashyadav8595411@gmail.com>
2026-02-25 19:12:20 +0530

    small bug saving user before generating the token

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,9 @@ import "dotenv/config";
 
 export const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const { MONGODB_URI } = process.env;
+        if(!MONGODB_URI) throw new Error("MONGODB_URI is not set");
+        const conn = await mongoose.connect(MONGODB_URI);
         console.log("MONGODB CONNECTED: ", conn.connection.host) 
     } catch (error) {
         console.log("Error connecting to MONGODB", error);
