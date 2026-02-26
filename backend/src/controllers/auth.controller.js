cbb74e1536d0dae68d8a1282bcf7b8ebfddac390
yashyadavMRU <yashyadav8595411@gmail.com>
2026-02-26 15:37:20 +0530

    unselect password

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -103,7 +103,7 @@ export const updateProfile = async(req, res) => {
 
         const uploadResponse = await cloudinary.uploader.upload(profilePic);
 
-        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true}).select("-password");
 
         res.status(200).json(updatedUser);
     } catch (error) {
