f67372de9561a2d28710f8007656f3f5a018f70a
yashyadavMRU <yashyadav8595411@gmail.com>
2026-02-27 06:42:12 +0530

    added rate limit using arcjet

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoutes } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
