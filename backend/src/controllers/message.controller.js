684ef862fd9fe53eff65e467409a8aa5727fdff5
yashyadavMRU <yashyadav8595411@gmail.com>
2026-02-27 13:08:56 +0530

    message route optimized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -59,6 +59,15 @@ export const sendMessage = async(req, res) => {
         const {text, image} = req.body;
         const { id: receiverId} = req.params;
         const senderId = req.user._id;
+
+        if(!text && !image) return res.status(400).json({ message: "Text or image is required"});
+
+        if(senderId.equals(receiverId)) return res.status(400).json({ message: "Cannot send messages to yourself"});
+
+        const receiverExists = await User.exists({ _id: receiverId });
+        if(!receiverExists) return res.status(400).json({ message: "Receiver no found"});
+
+
         let imageUrl;
         if(image){
             // upload base64 image to cloudinary
--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -13,6 +13,8 @@ const messageSchema = new mongoose.Schema({
     },
     text: {
         type: String,
+        trim: true,
+        maxlength: 2000,
     },
     image: {
         type: String,
